ae406d58fd25eeafd4b3b05d2f15cb61afe294a9
BrunoH <bruno.simpson10@hotmail.com>
2023-09-29 17:52:01 -0300

    Aula sobre criação e movimento de entidades

--- MountainShooter/code/Const.py
+++ MountainShooter/code/Const.py
@@ -1,3 +1,5 @@
+import pygame
+
 # C
 COLOR_ORANGE = (255, 128, 0)
 COLOR_YELLOW = (255, 255, 128)
@@ -7,13 +9,28 @@ COLOR_WHITE = (255, 255, 255)
 ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg1': 1,
                 'Level1Bg2': 2,
-                'Level1Bg3': 3}
+                'Level1Bg3': 3,
+                'Player1': 3,
+                'Player2': 3,
+                'Enemy1': 3}
+
+EVENT_ENEMY = pygame.USEREVENT + 1
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'EXIT')
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
 
 # W
 WIN_WIDTH = 576
